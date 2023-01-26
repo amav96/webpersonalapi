c48539094224a5465ef10e46560c3533768cc355
amav96 <alvaroamav96@gmail.com>
2023-01-26 11:46:16 -0300

    mnacro

--- constants.js
+++ constants.js
@@ -3,7 +3,7 @@ const DB_PASSWORD = "admin123456";
 const DB_HOST = "webpersonalapi.c8a1cyo.mongodb.net";
 
 const API_VERSION="v1";
-const IP_SERVER="34.197.229.45";
+const IP_SERVER="apiblog.macrocommerces";
 //
 const JWT_SECRET_KEY = "0bPssuvOcRmDN0OPklAWlg7r7.NdMbz";
 
