7c35409556184593b1059711a5f57ce7da4939b8
amav96 <alvaroamav96@gmail.com>
2023-01-25 23:01:17 -0300

    w80

--- constants.js
+++ constants.js
@@ -4,7 +4,7 @@ const DB_HOST = "webpersonalapi.c8a1cyo.mongodb.net";
 
 const API_VERSION="v1";
 const IP_SERVER="34.197.229.45";
-
+//
 const JWT_SECRET_KEY = "0bPssuvOcRmDN0OPklAWlg7r7.NdMbz";
 
 module.exports = {
